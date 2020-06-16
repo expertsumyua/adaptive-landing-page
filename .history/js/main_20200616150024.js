01ddc9dc477d18bbec6952f12239ca683e9c6fda
expertsumyua <expert.sumy.ua@gmail.com>
2020-06-16 15:02:23 +0300

    чистка кода

---
+++ .history/js/main_20200616150024.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let burger = document.querySelector("header .mobile img");
+let menu = document.querySelector("header nav");
+let burgerShow = false;
+let menuShow = true;
+let body = document.querySelector("body");
+burger.onclick = function () {
+
+	if (menu.style.display == "none" || menu.style.display == "") {
+		// if (!menuShow) {
+
+		// menuShow = true;
+		menu.style.margin = "50px";
+		menu.style.display = "block";
+	}
+	else {
+		// menuShow = false;
+		menu.style.display = "none";
+		menu.style.margin = "0px";
+	}
+}
+
+window.onresize = function () {
+	// if (menu.style.display == "none" || menu.style.display == "") { menuShow = false; }
+	// else { menuShow = true; }
+
+	if (body.offsetWidth <= 650) {
+		burgerShow = true;
+		menu.style.margin = "50px";
+	} else {
+		burgerShow = false;
+		menu.style.margin = "0px";
+	}
+
+	if (body.offsetWidth > 650 && !burgerShow) {
+		menu.style.display = "block";
+	}
+	else {
+		menu.style.display = "none";
+	}
+}
+
